1b68dd3a6034c3dc097490f73bcd4fbb37f5658e
GustavoSivestre <gusttavooscl@gmail.com>
2025-05-12 19:41:18 -0300

    salvo

--- cursoPhyton/a141_mixins_abstracao/log.py
+++ cursoPhyton/a141_mixins_abstracao/log.py
@@ -1,5 +1,9 @@
 # Abstração
 # Log
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
 
 class Log:
     def _log(self, msg):
@@ -14,7 +18,11 @@ class Log:
 
 class LogFileMixin(Log):
     def _log(self, msg):
-        print(msg)
+        msg_formatada = f'{msg} ({self.__class__.__name__})'
+        print('Salvando no log', msg_formatada)
+        with open(LOG_FILE, 'a') as arquivo:
+            arquivo.write(msg_formatada)
+            arquivo.write('\n')
 
 class LogPrintMixin(Log):
     def _log(self, msg):
@@ -22,6 +30,11 @@ class LogPrintMixin(Log):
 
 
 if __name__ == '__main__':
-    l = LogPrintMixin()
-    l.log_error('Qualquer coisa')
-    l.log_success('Que legal')
+    lp = LogPrintMixin()
+    lp.log_error('Qualquer coisa')
+    lp.log_success('Que legal')
+
+    lf = LogFileMixin()
+    lf.log_error('Qualquer coisa')
+    lf.log_success('Que legal')
+    # print(LOG_FILE)
