7144b94ed8dda98bc180fc4b1c613d7ce85a9dbb
GustavoSivestre <gusttavooscl@gmail.com>
2025-05-28 19:22:17 -0300

    salvo

--- cursoPhyton/a144_polimorfismo.py
+++ cursoPhyton/a144_polimorfismo.py
@@ -11,3 +11,38 @@
 # Princípio da substituição de liskov
 # Objetos de uma superclasse devem ser substituíveis
 # por objetos de uma subclasse sem quebrar a aplicação.
+# Sobrecarga de métodos (overload)  🐍 = ❌
+# Sobreposição de métodos (override) 🐍 = ✅
+
+from abc import ABC, abstractmethod
+
+class Notificacao(ABC):
+    def __init__(self, mensagem) -> None:
+        self.mensagem = mensagem
+
+    @abstractmethod
+    def enviar(self) -> bool:
+        ...
+
+class NotificacaoEmail(Notificacao):
+    def enviar(self) -> bool:
+        print('E-mail: enviando - ', self.mensagem)
+        return True
+
+class NotificacaoSMS(Notificacao):
+    def enviar(self) -> bool:
+        print('SMS: enviando - ', self.mensagem)
+        return False
+
+
+
+def notificar(notificacao: Notificacao):
+    notificacao_enviada = notificacao.enviar()
+
+    if notificacao_enviada:
+        print('Notificação enviada')
+    else:
+        print('Notificação não enviada')
+
+notificar(NotificacaoEmail('Testando email'))
+notificar(NotificacaoSMS('Testando sms'))
